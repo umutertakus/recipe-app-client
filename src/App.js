0855e96f7c2b8a7d453702533214f64efbe3d03b
umutertakus <umutertakus@hotmail.com>
2023-03-16 02:32:59 +0300

    Create routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Auth from "./pages/Auth";
+import CreateRecipe from "./pages/CreateRecipe";
+import Home from "./pages/Home";
+import SavedRecipes from "./pages/SavedRecipes";
+
 const App = () => {
-  return <div className="App">React App</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/create-recipe" element={<CreateRecipe />} />
+          <Route path="/saved-recipes" element={<SavedRecipes />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
