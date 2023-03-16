68a8e4d2ef655c820a3166c458c05717abb6e7e4
umutertakus <umutertakus@hotmail.com>
2023-03-16 23:55:01 +0300

    Add logout function

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,26 @@
 import { Link } from "react-router-dom";
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const [cookies, setCookies] = useCookies(["access_token"]);
+
+  const logout = () => {
+    setCookies("access_token", "");
+    localStorage.removeItem("userId");
+    navigate("/auth");
+  };
   return (
     <div className="navbar">
       <Link to="/">Home</Link>
       <Link to="/create-recipe">Create Recipe</Link>
       <Link to="/saved-recipes">Saved Recipes</Link>
-      <Link to="/auth">Login/Register</Link>
+      {!cookies.access_token ? (
+        <Link to="/auth">Login/Register</Link>
+      ) : (
+        <button onClick={logout}>Logout</button>
+      )}
     </div>
   );
 };
