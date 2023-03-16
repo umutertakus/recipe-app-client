16e69076c184b3464b1423a31433b8588e06069f
umutertakus <umutertakus@hotmail.com>
2023-03-16 23:50:24 +0300

    Update login function

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -2,8 +2,13 @@ import { useState } from "react";
 import Login from "../components/Login";
 import Register from "../components/Register";
 import axios from "axios";
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
 
 const Auth = () => {
+  const [_, setCookies] = useCookies(["access_token"]);
+  const navigate = useNavigate();
+
   const [userInfo, setUserInfo] = useState({
     username: "",
     password: "",
@@ -13,8 +18,15 @@ const Auth = () => {
   const loginSubmit = async (event) => {
     event.preventDefault();
     try {
-      await axios.post("http://localhost:3300/auth/login", userInfo);
+      const response = await axios.post(
+        "http://localhost:3300/auth/login",
+        userInfo
+      );
+      const { token, userId } = response.data;
+      setCookies("access_token", token);
+      localStorage.setItem("userId", userId);
       alert("Login successful.");
+      navigate("/");
     } catch (err) {
       console.error(err);
     }
@@ -25,6 +37,7 @@ const Auth = () => {
     try {
       await axios.post("http://localhost:3300/auth/register", userInfo);
       alert("Registration completed. You must login.");
+      setIsLoginPage(true);
     } catch (err) {
       console.error(err);
     }
