67e7eb0f4a699877e1d9f3a5070c00af764b4ad8
umutertakus <umutertakus@hotmail.com>
2023-03-17 00:45:01 +0300

    Create CreateRecipe page

--- src/pages/CreateRecipe.js
+++ src/pages/CreateRecipe.js
@@ -1,5 +1,58 @@
+import { useState } from "react";
+
 const CreateRecipe = () => {
-  return <div>CreateRecipe</div>;
+  const [recipe, setRecipe] = useState({
+    name: "",
+    ingredients: "",
+    instructions: "",
+    imageUrl: "",
+    cookingTime: 0,
+    userOwner: "",
+  });
+
+  const handleRecipeChange = (event) => {
+    const { id: name, value } = event.target;
+    setRecipe((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <div className="create-recipe">
+      <h2>Create Recipe</h2>
+      <form>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          value={recipe.name}
+          onChange={handleRecipeChange}
+        />
+        <label htmlFor="ingredients">Ingredients</label>
+        <label htmlFor="instructions">Instructions</label>
+        <textarea
+          id="instructions"
+          value={recipe.instructions}
+          onChange={handleRecipeChange}
+        />
+        <label htmlFor="imageUrl">Image URL</label>
+        <input
+          type="text"
+          id="imageUrl"
+          value={recipe.imageUrl}
+          onChange={handleRecipeChange}
+        />
+        <label htmlFor="cookingTime">Cooking Time (minutes)</label>
+        <input
+          type="text"
+          id="cookingTime"
+          value={recipe.cookingTime}
+          onChange={handleRecipeChange}
+        />
+      </form>
+    </div>
+  );
 };
 
 export default CreateRecipe;
