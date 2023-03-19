865816fea7f02e231e553ea37a719ce1995630de
umutertakus <umutertakus@hotmail.com>
2023-03-19 19:28:20 +0300

    Create Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,78 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import useGetUserId from "../hooks/useGetUserId";
+
 const Home = () => {
-  return <div>Home</div>;
+  const userId = useGetUserId();
+
+  const [recipes, setRecipes] = useState([]);
+  const [savedRecipes, setSavedRecipes] = useState([]);
+
+  const fetchRecipes = async () => {
+    try {
+      const response = await axios.get("http://localhost:3300/recipes");
+      setRecipes(response.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const fetchSavedRecipes = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:3300/recipes/savedRecipes/ids/${userId}`
+      );
+      setSavedRecipes(response.data.savedRecipes);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const saveRecipe = async (recipeId) => {
+    try {
+      const requestBody = {
+        recipeId,
+        userId,
+      };
+      const response = await axios.put(
+        "http://localhost:3300/recipes",
+        requestBody
+      );
+      setSavedRecipes(response.data.savedRecipes);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchRecipes();
+    fetchSavedRecipes();
+  }, []);
+
+  return (
+    <div>
+      <h1>Recipes</h1>
+      <ul>
+        {recipes.map((recipe) => (
+          <li key={recipe._id}>
+            <div>
+              <h2>{recipe.name}</h2>
+              {savedRecipes.includes(recipe._id) ? (
+                <p>Already saved</p>
+              ) : (
+                <button onClick={() => saveRecipe(recipe._id)}>Save</button>
+              )}
+            </div>
+            <div className="instructions">
+              <p>{recipe.instructions}</p>
+            </div>
+            <img src={recipe.imageUrl} alt={recipe.name} />
+            <p>Cooking Time: {recipe.cookingTime} (minutes)</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
